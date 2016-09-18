b21eee78ab6ca7bf83de32b72f90147695b7468a
Borin Ouch <borin.ouch@gmail.com>
2016-09-18 00:41:38 -0700

    Add resource holder

---
+++ resourceholder.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <memory>
+#include <exception>
+
+#include "resourceholder.h"
+
+template <typename R, typename I>
+void ResourceHolder<R, I>::load(I id, const std::string& filename) {
+    std::unique_ptr<R> resource(new R());
+    if (!resource->loadFromFile(filename)) {
+        throw std::runtime_error("ResourceHolder::load - Failed to load " + filename);
+    }
+
+    auto inserted = _resourceMap.insert(std::make_pair(id, std::move(resource)));
+    assert(inserted.second);
+}
+
+template <typename R, typename I>
+template <typename P>
+void ResourceHolder<R, I>::load(I id, const std::string& filename, const P& secondParam) {
+    std::unique_ptr<R> resource(new R());
+    if (!resource->loadFromFile(filename, secondParam)) {
+        throw std::runtime_error("ResourceHolder::load - Failed to load " + filename);
+    }
+
+    auto inserted = _resourceMap.insert(std::make_pair(id, std::move(resource)));
+    assert(inserted.second);
+}
+
+template <typename R, typename I>
+R& ResourceHolder<R, I>::get(I id) {
+    auto found = _resourceMap.find(id);
+    assert(found != _resourceMap.end());
+
+    return *found->second;
+}
+
+template <typename R, typename I>
+const R& ResourceHolder<R, I>::get(I id) const {
+    auto found = _resourceMap.find(id);
+    assert(found != _resourceMap.end());
+
+    return *found->second;
+}
+
---
+++ resourceholder.h
@@ -0,0 +1,28 @@
+#ifndef __RESOURCE_HOLDER_H__
+#define __RESOURCE_HOLDER_H__
+
+
+#include <string>
+#include <memory>
+
+#include <SFML/Graphics.hpp>
+
+
+template <typename Resource, typename Identifier>
+class ResourceHolder
+{
+public:
+    void load(Identifier id, const std::string& filename);
+
+    template <typename Parameter>
+    void load(Identifier id, const std::string& filename, const Parameter& secondParam);
+
+    Resource& get(Identifier id);
+    const Resource& get(Identifier id) const;
+
+private:
+    std::map<Identifier, std::unique_ptr<Resource>> _resourceMap;
+};
+
+
+#endif//__RESOURCE_HOLDER_H__
