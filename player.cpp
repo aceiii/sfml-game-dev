bb8773854e36a81fc4687e4356d14fc87e536e9b
Borin Ouch <borin.ouch@gmail.com>
2016-09-21 09:50:00 -0700

    Implement key binding

--- player.cpp
+++ player.cpp
@@ -4,6 +4,23 @@
 #include "aircraftmover.h"
 #include "derivedaction.h"
 
+Player::Player() {
+    _keyBinding[sf::Keyboard::Left] = MoveLeft;
+    _keyBinding[sf::Keyboard::Right] = MoveRight;
+
+    _actionBinding[MoveLeft].action = [=] (SceneNode& node, sf::Time dt) {
+        node.move(-playerSpeed * dt.asSeconds(), 0.0f);
+    };
+
+    _actionBinding[MoveRight].action = [=] (SceneNode& node, sf::Time dt) {
+        node.move(playerSpeed * dt.asSeconds(), 0.0f);
+    };
+
+    for (auto it = _actionBinding.begin(); it != _actionBinding.end(); it++) {
+        it->second.category = Category::PlayerAircraft;
+    }
+}
+
 void Player::handleEvent(const sf::Event &event, CommandQueue &commands) {
     if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P) {
         Command output;
@@ -16,12 +33,27 @@ void Player::handleEvent(const sf::Event &event, CommandQueue &commands) {
 }
 
 void Player::handleRealtimeInput(CommandQueue &commands) {
-    const float playerSpeed = 30.0f;
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-        Command moveLeft;
-        moveLeft.category = Category::PlayerAircraft;
-        moveLeft.action = derivedAction<Aircraft>(AircraftMover(-playerSpeed, 0.0f));
-        commands.push(moveLeft);
+    for (auto it = _keyBinding.begin(); it != _keyBinding.end(); it++) {
+        if (sf::Keyboard::isKeyPressed(it->first) && isRealtimeAction(it->second)) {
+            commands.push(_actionBinding[it->second]);
+        }
     }
+}
+
+void Player::assignKey(Player::Action action, sf::Keyboard::Key key) {
+    assert(_keyBinding.find(key) == _keyBinding.end());
+    _keyBinding[key] = action;
+}
+
+sf::Keyboard::Key Player::getAssignedKey(Player::Action action) const {
+    for (auto it = _keyBinding.begin(); it != _keyBinding.end(); it++) {
+        if (it->second == action) {
+            return it->first;
+        }
+    }
+    throw std::range_error("No key assigned to action");
+}
+
+bool Player::isRealtimeAction(Player::Action action) {
+    return true;
 }
--- player.h
+++ player.h
@@ -2,6 +2,7 @@
 #define __PLAYER_H__
 
 #include <SFML/Window/Event.hpp>
+#include <map>
 
 #include "commandqueue.h"
 
@@ -9,8 +10,28 @@
 class Player
 {
 public:
+    enum Action
+    {
+        MoveLeft,
+        MoveRight,
+    };
+
+    Player();
+
+    void assignKey(Action action, sf::Keyboard::Key key);
+    sf::Keyboard::Key getAssignedKey(Action action) const;
+
     void handleEvent(const sf::Event& event, CommandQueue& commands);
     void handleRealtimeInput(CommandQueue& commands);
+
+private:
+    static bool isRealtimeAction(Action action);
+
+private:
+    std::map<sf::Keyboard::Key, Action> _keyBinding;
+    std::map<Action, Command> _actionBinding;
+
+    const float playerSpeed = 30.0f;
 };
 
 
