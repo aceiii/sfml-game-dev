7bf82cb513f5e80f8197c145a69d812aa36a1035
Borin Ouch <borin.ouch@gmail.com>
2016-09-22 08:14:45 -0700

    Fix gamestate.h

--- gamestate.h
+++ gamestate.h
@@ -2,6 +2,7 @@
 #define __GAME_STATE_H__
 
 #include "state.h"
+#include "world.h"
 
 
 class GameState: public State
@@ -10,10 +11,13 @@ public:
     GameState(StateStack &stateStack, Context context);
 
     virtual void draw() override;
-
     virtual bool update(sf::Time deltaTime) override;
-
     virtual bool handleEvent(const sf::Event &event) override;
+
+private:
+    World _world;
+    Player& _player;
+
 };
 
 
