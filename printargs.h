58f73592db29786d9e32257371f5e777c2d4759c
Borin Ouch <borin.ouch@gmail.com>
2016-09-26 17:52:58 -0700

    Actually add printargs

--- printargs.h
+++ printargs.h
@@ -1,8 +1,28 @@
-//
-// Created by Borin Ouch on 2016-09-22.
-//
+#ifndef __PRINT_ARGS_H__
+#define __PRINT_ARGS_H__
 
-#ifndef SFML_TEST_PRINTARGS_H
-#define SFML_TEST_PRINTARGS_H
+#include <ostream>
 
-#endif //SFML_TEST_PRINTARGS_H
+
+struct args_print_context_t {
+    int argc;
+    char **argv;
+
+    args_print_context_t(int a, char* b[]):argc(a),argv(b) {}
+};
+
+args_print_context_t print_args(int argc, char *argv[]) {
+    return args_print_context_t(argc, argv);
+}
+
+std::ostream& operator<< (std::ostream& os, const args_print_context_t& a) {
+    os << "args: [";
+    for (int i = 0; i < a.argc; i += 1) {
+        os << "'" << a.argv[i] << "',";
+    }
+    os << "]";
+    return os;
+}
+
+
+#endif//__PRINT_ARGS_H__
