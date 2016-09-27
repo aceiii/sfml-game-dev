f66f1be23ef0922be3007fa6600807baa947d9f2
Borin Ouch <borin.ouch@gmail.com>
2016-09-26 18:41:49 -0700

    Implement LoadingState constructor

--- loadingstate.cpp
+++ loadingstate.cpp
@@ -1,4 +1,29 @@
 #include "loadingstate.h"
+#include "util.h"
+
+LoadingState::LoadingState(StateStack &stateStack, State::Context context):
+    State(stateStack, context)
+{
+    sf::RenderWindow& window = *context.window;
+    sf::Font& font = context.fonts->get(Fonts::Default);
+
+    _loadingText.setFont(font);
+    _loadingText.setString("Loading Resources");
+    centerOrigin(_loadingText);
+    _loadingText.setPosition(window.getSize().x / 2u, window.getSize().y / 2u + 50);
+
+    _progressBarBackground.setFillColor(sf::Color::White);
+    _progressBarBackground.setSize(sf::Vector2f(window.getSize().x - 20, 10));
+    _progressBarBackground.setPosition(10, _loadingText.getPosition().y + 40);
+
+    _progressBar.setFillColor(sf::Color(100, 100, 100));
+    _progressBar.setSize(sf::Vector2f(200, 10));
+    _progressBar.setPosition(10, _loadingText.getPosition().y + 40);
+
+    setCompletion(0.0f);
+
+    _loadingTask.execute();
+}
 
 void LoadingState::draw() {
 
@@ -11,3 +36,4 @@ bool LoadingState::update(sf::Time deltaTime) {
 bool LoadingState::handleEvent(const sf::Event &event) {
     return false;
 }
+
