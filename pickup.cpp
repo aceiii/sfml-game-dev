6437d5c8c319e8f29f7ccb5656b73a7af4cabb6a
Borin Ouch <borin.ouch@gmail.com>
2016-10-21 10:12:37 -0700

    Fix Pickup implementation

--- pickup.cpp
+++ pickup.cpp
@@ -1,21 +1,25 @@
 #include "pickup.h"
 #include "datatables.h"
+#include "util.h"
 
 namespace {
     std::vector<PickupData> g_table = initializePickupData();
 }
 
 Pickup::Pickup(Pickup::Type type, const TextureHolder &textures):
-    Entity(0)
+    Entity(0),
+    _type(type),
+    _sprite(textures.get(g_table[type].texture))
 {
+    centerOrigin(_sprite);
 }
 
 unsigned int Pickup::getCategory() const {
-    return SceneNode::getCategory();
+    return Category::Pickup;
 }
 
 sf::FloatRect Pickup::getBoundingRect() const {
-    return sf::FloatRect();
+    return getWorldTransform().transformRect(_sprite.getGlobalBounds());
 }
 
 void Pickup::apply(Aircraft &player) const {
