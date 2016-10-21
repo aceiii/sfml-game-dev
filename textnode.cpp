4d4b1a3e0248a96041b9d93f4091732dce9bdb14
Borin Ouch <borin.ouch@gmail.com>
2016-10-21 10:24:01 -0700

    Initialize members in TextNode

--- textnode.cpp
+++ textnode.cpp
@@ -1,7 +1,9 @@
 #include "textnode.h"
 #include "util.h"
 
-TextNode::TextNode(const FontHolder &fonts, const std::string &text) {
+TextNode::TextNode(const FontHolder &fonts, const std::string &text):
+    _text()
+{
     _text.setFont(fonts.get(Fonts::Main));
     _text.setCharacterSize(20);
     setString(text);
